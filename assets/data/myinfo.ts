982d42a7bf2f4e860b044caee6bda0b07ce1a50d
kaur9291 <71672214+kaur9291@users.noreply.github.com>
2021-04-16 23:41:17 -0400

    Add files via upload

---
+++ assets/data/myinfo.ts
@@ -0,0 +1,11 @@
+import { YourLoginName } from 'src/app/yourLoginName';
+
+const MYINFO: YourLoginName = {
+  assignmentTitle: 'Assignment 4',
+  name: 'Jasvir Kaur',
+  campus: 'Davis Campus',
+  loginName: 'kaur9291',
+  studentNumber: '991593229',
+};
+
+export { MYINFO };
